a9acbbf20fd25c781d050e545c8cf3eb359dc8e5
abdihusseinow@gmail.com <abdihusseinow@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-02-24 20:13:24 +0000

    created class Cone and its tests

---
+++ 3DSurfaceAreaCalculator/src/surfaceAreaCalculator/Cone.java
@@ -0,0 +1,31 @@
+package surfaceAreaCalculator;
+
+public class Cone 
+{
+	 public double c_radius;
+	 public double c_height;
+  Cone(double radius, double height)
+  {
+	  c_radius  = radius;
+	  c_height = height;
+  }
+  
+ public boolean equals(Object firstObject)
+ {
+	 Cone secondObject= (Cone)firstObject;
+	if ((c_radius - secondObject.c_radius)==(c_height - secondObject.c_height))
+		 return true;
+	 else
+		 return false;
+	 
+ }
+ public double calculateArea()
+ {
+	 double hypotenuse  = Math.sqrt(c_height * c_height + c_radius * c_radius);
+	 double lateralSurfaceArea = Math.PI * c_radius * hypotenuse;
+     double baseArea = Math.PI * c_radius * c_radius;
+     double resultArea = baseArea  + lateralSurfaceArea ;
+     return resultArea;
+}
+
+ }
