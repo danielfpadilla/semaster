202b805f46afab26c1a5c0151cba833c3393b3ef
ilya.tsyplenkov@gmail.com <ilya.tsyplenkov@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-02-26 14:55:17 +0000

    Added class "Pyramid" and tests for it

--- 3DVolumeAreaCalculator/src/Pyramid.java
+++ 3DVolumeAreaCalculator/src/Pyramid.java
@@ -11,4 +11,34 @@ public class Pyramid
 		this.height = hei;
 	}
 	public Pyramid(){};
+	public int GetEdge()
+	{
+		return this.edge;
+	}
+	public int GetHeight()
+	{
+		return this.height;
+	}
+	public int GetNumberOfEdge()
+	{
+		return this.NumberOfEdge;
+	}
+	public void SetEdge(int edge)
+	{
+		this.edge = edge;
+	}
+	public void SetHeight(int height)
+	{
+		this.height = height;
+	}
+	public void SetNumberOfEdge(int numberofedge)
+	{
+		this.NumberOfEdge = numberofedge;
+	}
+	public double CalculateVolume()
+	{
+		double BaseSquare = (this.NumberOfEdge/4) * Math.pow(this.edge, 2) * (1/Math.tan(Math.PI/this.NumberOfEdge));
+		double volume = (1/3) * this.height * BaseSquare;
+		return volume;
+	}
 }
