12359911a07e201dcbf30a2783056a0b02433789
ilya.tsyplenkov@gmail.com <ilya.tsyplenkov@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-02-27 12:55:28 +0000

    Added exception handling for Torus's parameters Created tests for exception throws and tests for exception handling

--- 3DVolumeAreaCalculator/src/Torus.java
+++ 3DVolumeAreaCalculator/src/Torus.java
@@ -1,4 +1,3 @@
-
 public class Torus 
 {
 	private int CircleRadius = 0;
@@ -25,9 +24,57 @@ public class Torus
 	{
 		this.TorusRadius = torus_rad;
 	}
+	public void ValidateTorusRadius() throws ParameterValueException
+	{
+		if(this.TorusRadius < 1)
+			throw new ParameterValueException(this.TorusRadius);
+		System.out.println("TorusRadius validation passed");
+	}
+	public void ValidateCircleRadius() throws ParameterValueException
+	{
+		if(this.CircleRadius < 1)
+			throw new ParameterValueException(this.CircleRadius);
+		System.out.println("CircleRadius validation passed");
+	}
+	public void ValidateCircleRadiusAndTorusRadiusDifference() throws ParameterValueException
+	{
+		if((this.CircleRadius - this.TorusRadius) > 0)
+			throw new ParameterValueException(this.CircleRadius);
+	}	
 	public double CalculateVolume()
 	{
-		double volume = 2 * Math.pow(Math.PI, 2) * TorusRadius * Math.pow(CircleRadius, 2);
+		try
+		{
+			this.ValidateTorusRadius();
+		}
+		catch (ParameterValueException pve)
+		{
+			System.out.println("You entered negative value for TorusRadius parameter:" + pve);
+			System.out.println("This value will be converted to positive with the same module");
+			this.TorusRadius = (-1) * this.TorusRadius;
+		}
+		try
+		{
+			this.ValidateCircleRadius();
+		}
+		catch (ParameterValueException pve)
+		{
+			System.out.println("You entered negative value for CircleRadius parameter:" + pve);
+			System.out.println("This value will be converted to positive with the same module");
+			this.CircleRadius = (-1) * this.CircleRadius;
+		}
+		try
+		{
+			this.ValidateCircleRadiusAndTorusRadiusDifference();
+		}
+		catch (ParameterValueException pve)
+		{
+			System.out.println("You entered value for CircleRadius parameter which more than TorusRadiusValue, it is wrong:" + pve);
+			System.out.println("This value will be converted to the same TorusRadiusValue : " + this.TorusRadius);
+			this.CircleRadius = this.TorusRadius;
+
+		}
+		double volume = 2 * Math.pow(Math.PI, 2) * this.TorusRadius * Math.pow(this.CircleRadius, 2);
 		return volume;
 	}
 	
