3a499aaf5eef0e928859b756a1e14e5e927d288f
abdihusseinow@gmail.com <abdihusseinow@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-03-01 16:13:03 +0000

    a working copy of branch of 3Dcalculator for Code review

--- Example/src/ExampleTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import junit.framework.Assert;
-
-public class ExampleTest {
-	@Test public void CanCreateExample() {
-		Example example = new Example();
-		Assert.assertNotNull(example);
-	}
-}
