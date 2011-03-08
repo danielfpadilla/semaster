f051be1ad6956b5d126e5debefc1b6e06fb8d2e2
anton.dergunov@gmail.com <anton.dergunov@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-03-08 13:31:25 +0000

    setUp/tearDown test cases

--- Example/src/ExampleTest.java
+++ Example/src/ExampleTest.java
@@ -1,9 +1,33 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import junit.framework.Assert;
 
 public class ExampleTest {
-	@Test public void CanCreateExample() {
-		Example example = new Example();
-		Assert.assertNotNull(example);
-	}
+    protected int m_value;
+
+    @Before
+    public void setUp() {
+	m_value = 10;
+    }
+
+    @Test
+    public void TestSetUp() {
+        Assert.assertEquals(10, m_value);
+    }
+
+    @After
+    public void tearDown() {
+	m_value = -1;
+    }
+
+    @Test
+    public void CanCreateExample() {
+	Example example = new Example();
+	Assert.assertNotNull(example);
+    }
 }
+
+
+
+
