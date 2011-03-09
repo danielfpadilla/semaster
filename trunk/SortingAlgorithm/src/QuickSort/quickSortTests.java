1197fe9d49e0ae37b95490d832ace08dacddc243
baluku24@gmail.com <baluku24@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-03-09 21:37:22 +0000

    Some changes made on the in the quicksort

---
+++ trunk/SortingAlgorithm/src/QuickSort/quickSortTests.java
@@ -0,0 +1,40 @@
+package QuickSort;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import QuickSort.SortAlgorithm.QuickSort;
+
+public class quickSortTests {
+	@Test 
+	public void canCreateQuickSort()
+	{
+		QuickSort quicksort = new QuickSort();
+		Assert.assertNotNull(quicksort);	}
+	@Test 
+	public void canCreateArrayWithElements()
+	{
+		int[] a = new int [5];
+		QuickSort quicksort  = new QuickSort();
+		Assert.assertNotNull(quicksort);
+	}
+	@Test
+	public void canInitializeArrayWithElements()
+	{
+		int[] a = {1, 2, 3, 4, 5};
+		
+		QuickSort quicksort  = new QuickSort();
+		Assert.assertEquals(a,quicksort.initializeArray(a));
+		
+	}
+	@Test
+	public void canSortAnUnsortedArray()
+	{
+		int [] unsorted  = {5, 4 , 3, 2, 1};
+		QuickSort quicksort = new QuickSort();
+		int [] actualsorted = quicksort.quickSortAlgorithm(unsorted); 
+		int [] expectedsorted = {1, 2, 3, 4, 5};
+		Assert.assertTrue(QuickSort.arrayEquals(expectedsorted ,actualsorted));
+	}
+}
