69b27c56dfdbdc029a4c46104a9e2e193c379509
jolie.sherry@gmail.com <jolie.sherry@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-03-16 18:24:42 +0000

    fixed AddMatrix created subtract test implemented AubtractMatrix

--- matrix/src/matrixoperations/test.java
+++ matrix/src/matrixoperations/test.java
@@ -3,10 +3,6 @@ package matrixoperations;
 import junit.framework.Assert;
 
 import org.junit.Test;
-
-
-
-
 public class test {
 	@Test public void canCreateMatrix()
 	{
@@ -21,26 +17,44 @@ public class test {
     	Assert.assertNotNull(operations);
     }
     
-    /*@Test public void canPrintMatrix()
+    @Test public void canPrintMatrix()
     {
     	double[][] a = {{1,2},{1,2}};
     	Matrix matrix=new Matrix(a);
-    	Assert.assertTrue(matrix.equals("{{1.0,2.0,0,},{1.0,2.0,0.0,},{0.0,0.0,0.0,},}"));
-    	
-    }*/
-    
-    @Test public void canAddTheMatrices()
-    {
-    	double[][] a = {{1,2},{1,2}};
-    	Matrix matA=new Matrix(a);
-    	//double [][] b = {{1,2},{1,2}};
-        Matrix matB=new Matrix(a);
-    	double [][] res = {{2,4},{2,4}};
-    	Matrix matRes = new Matrix(res);
-    	double [][]actual = {{0,0},{0,0}};
-    	Matrix actualres = new Matrix(actual);
-    	Assert.assertEquals(res,actualres.sum(a,a));
+    	Assert.assertEquals("{{1.0,2.0,},{1.0,2.0,},}",matrix.toString());
     	
     }
+     
+    @Test public void canAddMatrices()
+    {
+    	double[][] a = {{1,2},{1,2}};
+    	//Matrix object1=new Matrix(a);
+    	//Matrix object2=new Matrix(a);
+    	
+    	double [][]res = {{2,4},{2,4}};
+    	Matrix object3=new Matrix(res);
+    	
+    	Assert.assertEquals(object3, Matrix.sum(a,a));
+    	
+    	
+    	
+    }
+    @Test public void canSubtractMatrices()
+    {
+    	
+    	double [][] b={{2,2},{2,2}};
+    	double [][]c={{1,1},{1,1}};
+    	double [][]difference={{1,1},{1,1}};
+    	
+    	Matrix object4= new Matrix(difference);
+    	Assert.assertEquals(object4,Matrix.subtract(b,c));
+    	
+    	
+    }
+    
+    
+    
+    
+    
 
 }
