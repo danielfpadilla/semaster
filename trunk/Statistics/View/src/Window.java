c1d80b95664d80203791a76190c516903a35f5e8
kseniya.kornyakova@gmail.com <kseniya.kornyakova@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-04-05 13:31:01 +0000

    Added visualization for expectation,variance,third and fourth initial moments for equiprobability distribution

--- trunk/Statistics/View/src/Window.java
+++ trunk/Statistics/View/src/Window.java
@@ -7,7 +7,10 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.w3c.dom.css.ViewCSS;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.TableColumn;
@@ -15,19 +18,17 @@ import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.custom.TableTree;
 import org.eclipse.swt.custom.TableCursor;
 import org.eclipse.swt.widgets.TrayItem;
+import org.eclipse.swt.custom.TableEditor;
 
 
 
 public class Window extends View {
 
-	protected Shell shell;
-	private Text m_number1Text;
-	private Text m_number2Text;
+	protected  Shell shell;
 	private Text m_resultText;
 	private Text m_sizeText;
-	private Text m_probability1Text;
-	private Text m_probability2Text;
 	private Table table;
+	private Text valueText;
 
 	/**
 	 * Launch the application.
@@ -37,7 +38,7 @@ public class Window extends View {
 		try {
 			Window window = new Window();
 			new Presenter(window);
-			window.open();			
+			window.open();					
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -68,55 +69,12 @@ public class Window extends View {
 		shell.setSize(703, 534);
 		shell.setText("SWT Application");
 		
-		Label lblComplex = new Label(shell, SWT.NONE);
-		lblComplex.setBounds(384, 22, 46, 13);
-		lblComplex.setText("Number 1");
-		
-		m_number1Text = new Text(shell, SWT.BORDER);
-		m_number1Text.setBounds(436, 19, 76, 19);
-		
-		Label lblNumber = new Label(shell, SWT.NONE);
-		lblNumber.setBounds(384, 47, 46, 13);
-		lblNumber.setText("Number 2");
-		
-		m_number2Text = new Text(shell, SWT.BORDER);
-		m_number2Text.setBounds(436, 44, 76, 19);
-		
-		Button btnAdd = new Button(shell, SWT.BORDER | SWT.FLAT);
-		btnAdd.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				
-				m_number1 = m_number1Text.getText();
-				m_number2 = m_number2Text.getText();
-				m_handler.processAddAction();
-				m_resultText.setText(m_result);
-			}
-		});
-		btnAdd.setBounds(384, 90, 68, 23);
-		btnAdd.setText("Add");
-		
 		Label lblResult = new Label(shell, SWT.NONE);
-		lblResult.setBounds(507, 134, 49, 13);
+		lblResult.setBounds(391, 432, 49, 13);
 		lblResult.setText("Result:");
 		
 		m_resultText = new Text(shell, SWT.BORDER);
-		m_resultText.setBounds(569, 131, 76, 19);
-		
-		Button btnMultiply = new Button(shell, SWT.NONE);
-		btnMultiply.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e)
-			{
-				m_number1 = m_number1Text.getText();
-				m_number2 = m_number2Text.getText();
-				m_handler.processMultiplyAction();
-				m_resultText.setText(m_result);
-				
-			}
-		});
-		btnMultiply.setBounds(384, 129, 75, 25);
-		btnMultiply.setText("Multiply");
+		m_resultText.setBounds(456, 429, 76, 19);
 		
 		Button EquiprobabilityButton = new Button(shell, SWT.RADIO);
 		EquiprobabilityButton.addSelectionListener(new SelectionAdapter() {
@@ -145,11 +103,11 @@ public class Window extends View {
 		lblChoose.setText("Choose type of Distribution :");
 		
 		Label lblAdd = new Label(shell, SWT.NONE);
-		lblAdd.setBounds(32, 111, 127, 15);
+		lblAdd.setBounds(260, 20, 127, 15);
 		lblAdd.setText("Enter numbers of value :");
 		
 		m_sizeText = new Text(shell, SWT.BORDER);
-		m_sizeText.setBounds(163, 111, 49, 21);
+		m_sizeText.setBounds(260, 44, 49, 21);
 		
 				
 		Button ButtonOk = new Button(shell, SWT.NONE);
@@ -161,20 +119,26 @@ public class Window extends View {
 			{				
 				m_size = m_sizeText.getText();
 				size = Integer.parseInt(m_size);
-				m_array = new double[size];
-
-				TableItem[] tableItem;
+				m_array = new double[size];		
+				
+				TableItem[] tableItem ;
 				tableItem = new TableItem[size];
-			
 				for(int i = 0; i < size; i++)
 				{
 					tableItem[i] = new TableItem(table, SWT.NONE);
-				}	
-				tableItem[0].setText("Hello");
+				}		
+				m_temp = valueText.getText();
+				String[] value = m_temp.split("\\,");
+				
+				for(int i = 0; i < size; i++)
+				{
+					tableItem[i].setText(value[i]);
+				}
+			
 			}
 		});
 		
-		ButtonOk.setBounds(221, 111, 55, 23);
+		ButtonOk.setBounds(365, 69, 55, 23);
 		ButtonOk.setText("Ok");		
 		
 		
@@ -183,33 +147,87 @@ public class Window extends View {
 			
 
 			@Override
-			public void widgetSelected(SelectionEvent e) {
-				m_number1 = m_number1Text.getText();
-				m_number2 = m_number2Text.getText();
-				m_probability1 = m_probability1Text.getText();
-				m_probability2 = m_probability2Text.getText();
+			public void widgetSelected(SelectionEvent e)
+			{			
+				String[] value = m_temp.split("\\,");
+				m_array = new double[size];
+				for(int i = 0; i < size; i++)
+				{
+					m_array[i] = Double.parseDouble(value[i]);
+				}
 				m_handler.processExpectationAction();
-				m_resultText.setText(m_result);
+				m_resultText.setText(m_result);				
 			}
 		});
-		btnExpectation.setBounds(384, 170, 75, 25);
+		btnExpectation.setBounds(261, 153, 75, 25);
 		btnExpectation.setText("Expectation");
-		
-		m_probability1Text = new Text(shell, SWT.BORDER);
-		m_probability1Text.setBounds(556, 17, 76, 21);
-		
-		m_probability2Text = new Text(shell, SWT.BORDER);
-		m_probability2Text.setBounds(556, 47, 76, 21);
 		
 		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI);
 		table.setHeaderVisible(true);
 		table.setBounds(42, 169, 105, 249);
 		table.setLinesVisible(true);
-		table.getItems();
-		
+				
 		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
 		tblclmnNewColumn.setWidth(100);
-		tblclmnNewColumn.setText("Value");
+		tblclmnNewColumn.setText("Value");						
+		
+		valueText = new Text(shell, SWT.BORDER);
+		valueText.setBounds(260, 70, 76, 21);
+		
+		Button btnVariance = new Button(shell, SWT.NONE);
+		btnVariance.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				String[] value = m_temp.split("\\,");
+				m_array = new double[size];
+				for(int i = 0; i < size; i++)
+				{
+					m_array[i] = Double.parseDouble(value[i]);
+				}
+				m_handler.processVarianceAction();
+				m_resultText.setText(m_result);
+			}
+		});
+		btnVariance.setBounds(260, 184, 75, 25);
+		btnVariance.setText("Variance");
+		
+		Button btnThirdInitialMoment = new Button(shell, SWT.NONE);
+		btnThirdInitialMoment.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				String[] value = m_temp.split("\\,");
+				m_array = new double[size];
+				for(int i = 0; i < size; i++)
+				{
+					m_array[i] = Double.parseDouble(value[i]);
+				}
+				m_handler.processThirdInitialMomentAction();
+				m_resultText.setText(m_result);
+			}
+		});
+		btnThirdInitialMoment.setBounds(233, 215, 127, 25);
+		btnThirdInitialMoment.setText("Third Initial Moment");
+		
+		Button btnFourthInitialMoment = new Button(shell, SWT.NONE);
+		btnFourthInitialMoment.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				String[] value = m_temp.split("\\,");
+				m_array = new double[size];
+				for(int i = 0; i < size; i++)
+				{
+					m_array[i] = Double.parseDouble(value[i]);
+				}
+				m_handler.processFourthInitialMomentAction();
+				m_resultText.setText(m_result);
+			}
+		});
+		btnFourthInitialMoment.setBounds(233, 249, 127, 25);
+		btnFourthInitialMoment.setText("Fourth Initial Moment");
+
 
 	}
 }
