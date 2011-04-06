cebe70c4a4bd76034673dc9dd63a910a69b9e351
kseniya.kornyakova@gmail.com <kseniya.kornyakova@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-04-06 17:52:04 +0000

    Added some changes into the View

--- trunk/Statistics/Presentation/src/PresenterTest.java
+++ trunk/Statistics/Presentation/src/PresenterTest.java
@@ -10,32 +10,4 @@ public class PresenterTest
 		Assert.assertNotNull(presenter);
 	}
 
-	
-	
-	
-/*		
-	@Test public void canCalculateExpectation()
-	{
-		View view = new View();
-		Presenter presenter = new Presenter(view);
-		view.size = 2;
-		view.m_type = 1;
-		view.m_arrayOfValue = new double[view.size];
-		view.m_arrayOfValue[0] = 1.0;
-		view.m_arrayOfValue[1] = 2.0;
-		view.m_handler.processExpectationAction();
-		Assert.assertEquals(1.5, view.m_result);
-	}
-	
-	@Test public void canCalculateExpectationForDiscreteDistribution()
-	{
-		View view = new View();
-		Presenter presenter = new Presenter(view);
-		view.m_number1 = "1.0";
-		view.m_number2 = "2.0";
-		view.m_probability1 = "0.6";
-		view.m_probability2 = "0.4";
-		view.m_handler.processExpectationAction();
-		Assert.assertEquals("1.4", view.m_result);
-	}*/
 }
