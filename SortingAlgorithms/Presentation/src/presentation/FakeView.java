73779bc8f89319dd4636e966b8a08a6b4ad1387d
anton.dergunov@gmail.com <anton.dergunov@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-04-09 06:43:03 +0000

    Initial import.

---
+++ SortingAlgorithms/Presentation/src/presentation/FakeView.java
@@ -0,0 +1,55 @@
+package presentation;
+
+public class FakeView implements Iview
+{
+	public String m_unsortedArray;
+	public String m_sortedArray;
+	public String m_sortingMethod;
+	public String m_size;
+	public IActionHandler m_sortingArray;
+	public IActionHandler m_generateArrayHandler;
+	@Override
+	public String getUnsortedArray()
+	{
+		return m_unsortedArray;
+	}
+
+	@Override
+	public void setSortedArray(String sortedArray)
+	{
+	  m_sortedArray = sortedArray;
+
+	}
+
+	@Override
+	public String getArraySize()
+	{
+		return m_size;
+	}
+	@Override
+	public String getSortingMethod()
+	{
+		return m_sortingMethod;
+	}
+	@Override
+	public void generateRandomArray(IActionHandler handler)
+	{
+		m_generateArrayHandler = handler;
+
+	}
+
+	@Override
+	public void sortArray(IActionHandler handler)
+	{
+		m_sortingArray = handler;
+	}
+
+	@Override
+	public void setUnsortedArray(String sortedArray)
+	{
+        return;
+	}
+
+	
+
+}
