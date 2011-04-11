c22e2692e0d1cb22b6b009adc72eb24655286758
shabee.bakari@gmail.com <shabee.bakari@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-04-11 18:04:12 +0000

    handled exceptions

--- trunk/View/src/FractionCalculator.java
+++ trunk/View/src/FractionCalculator.java
@@ -4,9 +4,11 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class FractionCalculator implements IFractionView
 {
@@ -22,15 +24,21 @@ public class FractionCalculator implements IFractionView
 	private IActionHandler m_multiplyActionHandler;
 	private IActionHandler m_divideActionHandler;
 	private IActionHandler m_clearActionHandler;
-	
-	
-	
+	private IActionHandler m_inputActionHandler;
+	Button addButton;
+	Button substractButton;
+	Button MultiplyButton;
+	Button DivideButton;
+	private Label myImage;
+	private Label errorLabel;
+
 	{
-		
+
 	}
 
 	/**
 	 * Launch the application.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args)
@@ -71,82 +79,111 @@ public class FractionCalculator implements IFractionView
 	protected void createContents()
 	{
 		shlFractionCalculator = new Shell();
+		shlFractionCalculator.setImage(null);
 		shlFractionCalculator.setSize(450, 300);
 		shlFractionCalculator.setText("Fraction Calculator");
-		
+
+		ModifyListener listener = new ModifyListener()
+		{
+			public void modifyText(ModifyEvent e)
+			{
+				valueChanged((Text) e.widget);
+			}
+
+			public void valueChanged(Text text)
+			{
+				m_inputActionHandler.processAction();
+			}
+
+		};
+
 		Label m_numerator1 = new Label(shlFractionCalculator, SWT.NONE);
-		m_numerator1.setBounds(25, 61, 64, 20);
+		m_numerator1.setBounds(25, 42, 64, 20);
 		m_numerator1.setText("numerator1");
-		
+
 		Label m_denominator1 = new Label(shlFractionCalculator, SWT.NONE);
-		m_denominator1.setBounds(25, 93, 64, 14);
+		m_denominator1.setBounds(25, 68, 64, 14);
 		m_denominator1.setText("denominator1");
-		
+
 		m_numerator1Text = new Text(shlFractionCalculator, SWT.BORDER);
-		m_numerator1Text.setBounds(109, 61, 76, 20);
-		
+		m_numerator1Text.setBounds(109, 42, 76, 20);
+		m_numerator1Text.addModifyListener(listener);
+
 		m_denominator1Text = new Text(shlFractionCalculator, SWT.BORDER);
-		m_denominator1Text.setBounds(109, 87, 76, 20);
-		
+		m_denominator1Text.setBounds(109, 65, 76, 20);
+		m_denominator1Text.addModifyListener(listener);
+
 		Label m_numerator2 = new Label(shlFractionCalculator, SWT.NONE);
-		m_numerator2.setBounds(253, 61, 76, 20);
+		m_numerator2.setBounds(253, 42, 76, 20);
 		m_numerator2.setText("numerator2");
-		
+
 		Label m_denominator2 = new Label(shlFractionCalculator, SWT.NONE);
-		m_denominator2.setBounds(253, 93, 76, 14);
+		m_denominator2.setBounds(253, 68, 76, 14);
 		m_denominator2.setText("denominator2");
-		
+
 		m_numerator2Text = new Text(shlFractionCalculator, SWT.BORDER);
-		m_numerator2Text.setBounds(335, 61, 76, 20);
-		
+		m_numerator2Text.setBounds(335, 39, 76, 20);
+		m_numerator2Text.addModifyListener(listener);
+
 		m_denominator2Text = new Text(shlFractionCalculator, SWT.BORDER);
-		m_denominator2Text.setBounds(335, 87, 76, 20);
-		
-		Button addButton = new Button(shlFractionCalculator, SWT.NONE);
-		addButton.addSelectionListener(new SelectionAdapter() {
+		m_denominator2Text.setBounds(335, 65, 76, 20);
+		m_denominator2Text.addModifyListener(listener);
+
+		addButton = new Button(shlFractionCalculator, SWT.NONE);
+		addButton.addSelectionListener(new SelectionAdapter()
+		{
 			@Override
-			public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(SelectionEvent e)
+			{
 				m_addActionHandler.processAction();
 			}
-			
+
 		});
-		addButton.setBounds(43, 152, 69, 24);
+		addButton.setBounds(25, 109, 69, 24);
 		addButton.setText("Add");
-		
-		Button substractButton = new Button(shlFractionCalculator, SWT.NONE);
-		substractButton.addSelectionListener(new SelectionAdapter() {
+
+		substractButton = new Button(shlFractionCalculator, SWT.NONE);
+		substractButton.addSelectionListener(new SelectionAdapter()
+		{
 			@Override
-			public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(SelectionEvent e)
+			{
 				m_substractActionHandler.processAction();
 			}
 		});
-		substractButton.setBounds(43, 199, 69, 24);
+		substractButton.setBounds(25, 152, 69, 24);
 		substractButton.setText("Substract");
-		
-		Button MultiplyButton = new Button(shlFractionCalculator, SWT.NONE);
-		MultiplyButton.addSelectionListener(new SelectionAdapter() {
+
+		MultiplyButton = new Button(shlFractionCalculator, SWT.NONE);
+		MultiplyButton.addSelectionListener(new SelectionAdapter()
+		{
 			@Override
-			public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(SelectionEvent e)
+			{
 				m_multiplyActionHandler.processAction();
 			}
 		});
-		MultiplyButton.setBounds(279, 152, 69, 24);
+		MultiplyButton.setBounds(288, 109, 69, 24);
 		MultiplyButton.setText("Multiply");
-		
-		Button DivideButton = new Button(shlFractionCalculator, SWT.NONE);
-		DivideButton.addSelectionListener(new SelectionAdapter() {
+
+		DivideButton = new Button(shlFractionCalculator, SWT.NONE);
+		DivideButton.addSelectionListener(new SelectionAdapter()
+		{
 			@Override
-			public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(SelectionEvent e)
+			{
 				m_divideActionHandler.processAction();
 			}
 		});
-		DivideButton.setBounds(279, 199, 69, 24);
+		DivideButton.setBounds(288, 152, 69, 24);
 		DivideButton.setText("Divide");
-		
+
 		Button clearButton = new Button(shlFractionCalculator, SWT.NONE);
-		clearButton.addSelectionListener(new SelectionAdapter() {
+		clearButton.addSelectionListener(new SelectionAdapter()
+		{
 			@Override
-			public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(SelectionEvent e)
+			{
 				m_clearActionHandler.processAction();
 				m_numerator1Text.setText("0");
 				m_numerator2Text.setText("0");
@@ -154,114 +191,165 @@ public class FractionCalculator implements IFractionView
 				m_denominator2Text.setText("0");
 			}
 		});
-		clearButton.setBounds(157, 178, 69, 24);
+		clearButton.setBounds(153, 129, 69, 24);
 		clearButton.setText("Clear");
-		
+
 		m_result = new Text(shlFractionCalculator, SWT.BORDER);
-		m_result.setBounds(146, 10, 183, 20);
-		
-		
+		m_result.setBounds(135, 10, 183, 20);
+		m_result.setEditable(false);
+
+		myImage = new Label(shlFractionCalculator, SWT.NONE);
+		myImage.setImage(SWTResourceManager.getImage(FractionCalculator.class,
+				"/com/sun/java/swing/plaf/windows/icons/Error.gif"));
+		myImage.setBounds(60, 206, 34, 32);
+		myImage.setVisible(false);
+
+		errorLabel = new Label(shlFractionCalculator, SWT.NONE);
+		errorLabel.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
+		errorLabel.setBounds(109, 216, 206, 20);
 	}
 
 	@Override
 	public String getNumerator1()
 	{
-				return m_numerator1Text.getText();
+		return m_numerator1Text.getText();
 	}
 
 	@Override
 	public String getDenominator1()
 	{
-				return m_denominator1Text.getText();
+		return m_denominator1Text.getText();
 	}
 
 	@Override
 	public String getNumerator2()
 	{
-				return m_numerator2Text.getText();
+		return m_numerator2Text.getText();
 	}
 
 	@Override
 	public String getDenominator2()
 	{
-				return m_denominator2Text.getText();
+		return m_denominator2Text.getText();
 	}
 
 	@Override
 	public void setResult(String string)
 	{
 		m_result.setText(string);
-		
+
 	}
 
 	@Override
 	public void setAddActionHandler(IActionHandler handler)
 	{
-		m_addActionHandler=handler;
-		
-		
+		m_addActionHandler = handler;
+
 	}
 
 	@Override
 	public void setSubstractActionHandler(IActionHandler handler)
 	{
-		m_substractActionHandler=handler;
-		
+		m_substractActionHandler = handler;
+
 	}
 
 	@Override
 	public void setMultiplyActionHandler(IActionHandler handler)
 	{
-		m_multiplyActionHandler=handler;
-		
+		m_multiplyActionHandler = handler;
+
 	}
 
 	@Override
 	public void setDevideActionHandler(IActionHandler handler)
 	{
-		m_divideActionHandler=handler;
-		
+		m_divideActionHandler = handler;
+
 	}
 
 	@Override
 	public void setNumerator1(String string)
 	{
-	  m_numerator1Text.setText(string);
-		
+		m_numerator1Text.setText(string);
+
 	}
 
 	@Override
 	public void setDenominator1(String string)
 	{
 		m_denominator1Text.setText(string);
-		
+
 	}
 
 	@Override
 	public void setNumerator2(String string)
 	{
 		m_numerator2Text.setText(string);
-		
+
 	}
 
 	@Override
 	public void setDenominator2(String string)
 	{
 		m_denominator2Text.setText(string);
-		
+
 	}
 
 	@Override
 	public void setClearActionHandler(IActionHandler handler)
 	{
-		m_clearActionHandler=handler;
-		
+
+		m_clearActionHandler = handler;
+
 	}
 
 	@Override
 	public void setOutputError(String message)
 	{
-		m_result.setText(message);
-		
+		myImage.setVisible(true);
+		errorLabel.setVisible(true);
+		errorLabel.setText(message);
+
+	}
+
+	@Override
+	public void setInputActionHandler(IActionHandler handler)
+	{
+		m_inputActionHandler = handler;
+	}
+
+	@Override
+	public void buttonAdd(boolean flag)
+	{
+		addButton.setEnabled(flag);
+
+	}
+
+	@Override
+	public void buttonSubstract(boolean flag)
+	{
+		substractButton.setEnabled(flag);
+
+	}
+
+	@Override
+	public void buttonDivide(boolean flag)
+	{
+		DivideButton.setEnabled(flag);
+
+	}
+
+	@Override
+	public void buttonMultiply(boolean flag)
+	{
+		MultiplyButton.setEnabled(flag);
+
+	}
+
+	@Override
+	public void setimageLabel(boolean flag)
+	{
+		myImage.setVisible(flag);
 	}
 }
