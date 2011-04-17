16125ad46041806f4f881614a0423cc98b7614c9
shabee.bakari@gmail.com <shabee.bakari@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-04-17 09:42:53 +0000

    3

---
+++ Fraction/Presenter/src/IFractionView.java
@@ -0,0 +1,46 @@
+
+public interface IFractionView
+{
+	public String getNumerator1();
+
+	public String getDenominator1();
+
+	public String getNumerator2();
+
+	public String getDenominator2();
+
+	public void setNumerator1(String string);
+
+	public void setDenominator1(String string);
+
+	public void setNumerator2(String string);
+
+	public void setDenominator2(String string);
+
+	public void setResult(String string);
+
+	public void setAddActionHandler(IActionHandler handler);
+
+	public void setSubstractActionHandler(IActionHandler handler);
+
+	public void setMultiplyActionHandler(IActionHandler handler);
+
+	public void setDevideActionHandler(IActionHandler handler);
+
+	public void setInputActionHandler(IActionHandler handler);
+
+	public void setClearActionHandler(IActionHandler handler);
+
+	public void setOutputError(String message);
+
+	public void buttonAdd(boolean flag);
+
+	public void buttonSubstract(boolean flag);
+
+	public void buttonDivide(boolean flag);
+
+	public void buttonMultiply(boolean flag);
+
+	public void setimageLabel(boolean flag);
+
+}
