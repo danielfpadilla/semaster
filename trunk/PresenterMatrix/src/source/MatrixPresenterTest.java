0581cde948e194ce62a63cdb071709cfa84f4d5a
jolie.sherry@gmail.com <jolie.sherry@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-04-18 10:41:25 +0000

    added inverse and multiplication

--- trunk/PresenterMatrix/src/source/MatrixPresenterTest.java
+++ trunk/PresenterMatrix/src/source/MatrixPresenterTest.java
@@ -31,8 +31,8 @@ public class MatrixPresenterTest
 	{
 		double[][] a =
 		{
-		{ 1.0, 1.0 },
-		{ 1.0, 1.0 } };
+				{ 1.0, 1.0 },
+				{ 1.0, 1.0 } };
 		double[][] b = new double[2][2];
 		Matrix object1 = new Matrix(a, 2);
 		Matrix object2 = new Matrix(b, 2);
@@ -47,31 +47,90 @@ public class MatrixPresenterTest
 	}
 
 	@Test
-public void canAddTwoMatrices()
-{
+	public void canAddTwoMatrices()
+	{
 
-	m_view.m_A00 = "1.0";
-	m_view.m_A01 = "1.0";
-	m_view.m_A10 = "1.0";
-	m_view.m_A11 = "1.0";
-	m_view.m_B00 = "1.0";
-	m_view.m_B01 = "1.0";
-    m_view.m_B10 = "1.0";
-	m_view.m_B11 = "1.0";
-	m_view.m_addMatrixActionHandler.processAction();
-	Assert.assertEquals("2.0",m_view.m_c00);
-	Assert.assertEquals("2.0",m_view.m_c01);
-	Assert.assertEquals("2.0",m_view.m_c10);
-	Assert.assertEquals("2.0",m_view.m_c11);
-}
-@ Test
-public void canCalculateDeterminantOfAMatrix()
-{
-	m_view.m_A00 = "2.0";
-	m_view.m_A01 = "1.0";
-	m_view.m_A10 = "5.0";
-	m_view.m_A11 = "1.0";
-	m_view.m_determinantActionHandler.processAction();
-	Assert.assertEquals("-3.0",m_view.m_determinantResult);
-}
+		m_view.m_A00 = "1.0";
+		m_view.m_A01 = "1.0";
+		m_view.m_A10 = "1.0";
+		m_view.m_A11 = "1.0";
+
+		m_view.m_B00 = "1.0";
+		m_view.m_B01 = "1.0";
+		m_view.m_B10 = "1.0";
+		m_view.m_B11 = "1.0";
+		m_view.m_addMatrixActionHandler.processAction();
+		Assert.assertEquals("2.0",m_view.m_c00);
+		Assert.assertEquals("2.0",m_view.m_c01);
+		Assert.assertEquals("2.0",m_view.m_c10);
+		Assert.assertEquals("2.0",m_view.m_c11);
+	}
+	
+	@Test
+	public void canSubtractMatrices()
+	{
+
+		m_view.m_A00 = "2.0";
+		m_view.m_A01 = "2.0";
+		m_view.m_A10 = "2.0";
+		m_view.m_A11 = "2.0";
+
+		m_view.m_B00 = "1.0";
+		m_view.m_B01 = "1.0";
+		m_view.m_B10 = "1.0";
+		m_view.m_B11 = "1.0";
+		m_view.m_subtractActionHandler.processAction();
+		Assert.assertEquals("1.0",m_view.m_c00);
+		Assert.assertEquals("1.0",m_view.m_c01);
+		Assert.assertEquals("1.0",m_view.m_c10);
+		Assert.assertEquals("1.0",m_view.m_c11);
+	}
+	@ Test
+	public void canCalculateDeterminantOfAMatrix()
+	{
+		m_view.m_A00 = "2.0";
+		m_view.m_A01 = "1.0";
+		m_view.m_A10 = "5.0";
+		m_view.m_A11 = "1.0";
+		m_view.m_determinantActionHandler.processAction();
+		Assert.assertEquals("-3.0",m_view.m_determinantResult);
+	}
+	@Test 
+	public void canMultiplyMatrices()
+	{
+		m_view.m_A00 ="1.0";
+		m_view.m_A01 = "2.0";
+		m_view.m_A10 = "3.0";
+		m_view.m_A11 = "1.0";
+
+		m_view.m_B00 = "2.0";
+		m_view.m_B01 = "1.0";
+		m_view.m_B10 = "2.0";
+		m_view.m_B11 = "4.0";
+
+		m_view.m_multiplyActionHandler.processAction();
+		Assert.assertEquals("6.0",m_view.m_c00);
+		Assert.assertEquals("9.0",m_view.m_c01);
+		Assert.assertEquals("8.0",m_view.m_c10);
+		Assert.assertEquals("7.0",m_view.m_c11);
+
+
+	}
+	@Test
+	public void canFindInverseOfMatrix()
+	{
+		m_view.m_A00 ="4.0";
+		m_view.m_A01 = "2.0";
+		m_view.m_A10 = "2.0";
+		m_view.m_A11 = "2.0";
+		
+		m_view.m_inverseActionHandler.processAction();
+		Assert.assertEquals("0.5",m_view.m_c00);
+		Assert.assertEquals("-0.5",m_view.m_c01);
+		Assert.assertEquals("-0.5",m_view.m_c10);
+		Assert.assertEquals("1.0",m_view.m_c11);
+
+
+	}
+
 }
