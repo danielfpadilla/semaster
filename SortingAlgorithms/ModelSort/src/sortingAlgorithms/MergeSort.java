e646cb400fa2d394673731c71df26b40e8174ebe
baluku24@gmail.com <baluku24@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-04-19 22:14:17 +0000

    ModelSort Revision

---
+++ SortingAlgorithms/ModelSort/src/sortingAlgorithms/MergeSort.java
@@ -0,0 +1,78 @@
+package sortingAlgorithms;
+
+public class MergeSort extends Sorting
+{
+	private int numberOfElements;
+	public int[] numbers = new int[numberOfElements];
+
+	@Override
+	
+	public String getSortingName()
+	{
+		return "MergeSort";
+	}
+
+	@Override
+	public int[] sortInternal(int[] values)
+	{
+		this.numbers = values;
+		numberOfElements = values.length;
+
+		mergeSort(0, numberOfElements - 1);
+		return values;
+	}
+
+	private void mergeSort(int low, int high)
+	{
+		if (low < high)
+		{
+			int middle = (low + high) / 2;
+			mergeSort(low, middle);
+			mergeSort(middle + 1, high);
+			merge(low, middle, high);
+		}
+	}
+
+	private void merge(int low, int middle, int high)
+	{
+
+		int[] helperArray = new int[numberOfElements];
+		for (int i = low; i <= high; i++)
+		{
+			helperArray[i] = numbers[i];
+		}
+
+		int i = low;
+		int j = middle + 1;
+		int k = low;
+		while (i <= middle && j <= high)
+		{
+			if (helperArray[i] <= helperArray[j])
+			{
+				numbers[k] = helperArray[i];
+				i++;
+			} else
+			{
+				numbers[k] = helperArray[j];
+				j++;
+			}
+			k++;
+		}
+		while (i <= middle)
+		{
+			numbers[k] = helperArray[i];
+			k++;
+			i++;
+		}
+		helperArray = null;
+
+	}
+
+	public int[] sortArray(int[] unsorted) 
+	{
+		return null;
+	}
+
+	
+
+}
