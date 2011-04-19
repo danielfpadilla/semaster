42ea6a58cd842edd592bcfa3db09e4d1fee05c44
baluku24@gmail.com <baluku24@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-04-19 14:58:51 +0000

    Revision of presentationSort

---
+++ trunk/SortingAlgorithms/PresentationSort/src/presentation/PresenterTests.java
@@ -0,0 +1,30 @@
+package presentation;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PresenterTests
+{
+	FakeView m_view;
+	Presenter m_presenter;
+	@Before
+	public void setUp()
+	{
+		m_view = new FakeView();
+		m_presenter = new Presenter(m_view);
+	}
+
+	@After
+	public void tearDown()
+	{
+		m_view = null;
+		m_presenter = null;
+	}
+	@Test
+	public void canSortingTakePlace()
+	{
+		
+	}
+
+}
