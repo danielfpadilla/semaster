948a3e14e2699ff412d9157ba3bc607f7e9b04f7
abdihusseinow@gmail.com <abdihusseinow@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-04-21 07:17:41 +0000

    latests model code

---
+++ trunk/Projects/Hussein/3DFigureAreaCalculator/Model/src/figurearea/model/SquareBasedPyramid.java
@@ -0,0 +1,35 @@
+package figurearea.model;
+
+public class SquareBasedPyramid implements I3DFigure
+{
+	private double m_baseLength;
+	private double m_height;
+	private final int NUM_OF_SIDES = 2;
+
+	public SquareBasedPyramid(double baseLength, double height)
+	{
+		if (baseLength <= 0.0 || height <= 0.0)
+			throw new IllegalArgumentException("Negative baseLength or Height");
+		m_baseLength = baseLength;
+		m_height = height;
+	}
+
+	public double getBaselength()
+	{
+		return m_baseLength;
+	}
+
+	public double getHeight()
+	{
+		return m_height;
+	}
+
+	public double calculateArea()
+	{
+		double slantHeight = Math.hypot(0.5 * m_baseLength, m_height);
+		double baseArea = Math.pow(m_baseLength, 2);
+		double lateralArea = NUM_OF_SIDES * m_baseLength * slantHeight;
+		double result = lateralArea + baseArea;
+		return result;
+	}
+}
