de6aac89484e623d3db23f02255fd3e4e07daae2
jolie.sherry@gmail.com <jolie.sherry@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-11-21 19:15:01 +0000

    number four

---
+++ checkers/View/src/edu/semaster/checkers/view/Position.java
@@ -0,0 +1,33 @@
+package edu.semaster.checkers.view;
+
+public class Position
+{
+	
+		public int x;
+		public int y;
+
+		public Position (){}
+		public Position (int x,int y)
+		{ 
+		this.x=x;
+		this.y=y;
+		}
+
+		public void setx(int x)
+		{ 
+			this.x=x;	
+		}
+		public void sety(int y)
+		{ 
+			this.y=y;	
+		}
+		public int getx()
+		{ 
+			return x;	
+		}
+		public int gety( )
+		{ 
+			return y;	
+		}
+
+}
--- checkers/View/src/edu/semaster/checkers/view/View.java
+++ checkers/View/src/edu/semaster/checkers/view/View.java
@@ -5,113 +5,209 @@ import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseMoveListener;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 
-public class View
-{
-
+public class View {
+	int BOARD_OFFSET_X = 0;
+	int BOARD_OFFSET_Y = 0;
+	int FIGURE_HEIGHT ;
+	int FIGURE_WIDTH;
 	protected Shell shell;
+	static int[][] m_board;
 
-	/**
-	 * Launch the application.
-	 * @param args
-	 */
-	public static void main(String[] args)
+	public static void main(String[] args) {
+	try {
+	View window = new View();
+	window.open();
+	} catch (Exception e) {
+	e.printStackTrace();
+	}
+
+	final Display display = new Display();
+	Shell shell = new Shell(display);
+	shell.addListener(SWT.Paint, new Listener() {
+		
+	public void initializeBoard() 
 	{
-		try
-		{
-			View window = new View();
-			window.open();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+	for (int i = 0; i < 8; i++)
+	  for (int j = 0; j < 8; j++)
+	     m_board[i][j] = getImagePositions(i, j);
+        }
 
-	/**
-	 * Open the window.
-	 */
-	public void open() {
-		Display display = Display.getDefault();
-		createContents();
-		Image image = new Image(display,
-				View.class.getResourceAsStream("board.jpg"));
-		
-		Image redFigure = new Image(display,
-				View.class.getResourceAsStream("red_normal.jpg"));
-		GC gc = new GC(image);
-		gc.drawImage(redFigure, 22, 20);
-		gc.drawImage(redFigure, 138, 20);
-		gc.drawImage(redFigure, 254, 20);
-		gc.drawImage(redFigure, 370, 20);
+	private int getImagePositions(int x, int y) 
+	{
 
-		gc.drawImage(redFigure, 80, 80);
-		gc.drawImage(redFigure, 196, 80);
-		gc.drawImage(redFigure, 310, 80);
-		gc.drawImage(redFigure, 427, 80);
+	     if (x < 4) 
+	      {
+	        if ((x % 2 == 0 && y % 2 != 0)
+	        || (x % 2 != 0 && y % 2 == 1))
+	        {
+	         return 1;
+	        }
+              }
 
-		gc.drawImage(redFigure, 22, 140);
-		gc.drawImage(redFigure, 138, 140);
-		gc.drawImage(redFigure, 254, 140);
-		gc.drawImage(redFigure, 370, 140);
+	      if (x > 5) 
+	       {
 
-		Button button = new Button(shell, SWT.PUSH);
-		button.setImage(redFigure);
-		
+	         if ((x % 2 == 0 && y % 2 != 0)
+	         || (x % 2 != 0 && y % 2 == 0)) 
+	         {
+	         return 2;
+                 }
+	        }
 	
-		
-		Image yellowFigure = new Image(display,
-				View.class.getResourceAsStream("yellow_normal.jpg"));
-		gc.drawImage(yellowFigure, 80, 430);
-		gc.drawImage(yellowFigure, 196, 430);
-		gc.drawImage(yellowFigure, 312, 430);
-		gc.drawImage(yellowFigure, 428, 430);
+	      return 0;
+	}
 
-		gc.drawImage(yellowFigure, 22, 370);
-		gc.drawImage(yellowFigure, 138, 370);
-		gc.drawImage(yellowFigure, 254, 370);
-		gc.drawImage(yellowFigure, 370, 370);
-
-		gc.drawImage(yellowFigure, 80, 310);
-		gc.drawImage(yellowFigure, 196, 310);
-		gc.drawImage(yellowFigure, 312, 310);
-		gc.drawImage(yellowFigure, 428, 310);
-
-		shell.addMouseMoveListener(new MouseMoveListener() 
-		{
-
-			public void mouseMove(MouseEvent arg0) 
-			{
-				System.out.println(arg0.x + " , " + arg0.y);
-
-			}
-		});
-
-		gc.dispose();
-
-		shell.setBackgroundImage(image);
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed())
-		{
-			if (!display.readAndDispatch()) 
-			{
-				display.sleep();
-			}
-		}
+	@Override
+	public void handleEvent(Event e) {
+	
+	 GC gc = e.gc; 
+	for (int i = 0; i < 8; i++) 
+	   for (int j = 0; j <8; j++)
+		   gc.drawImage(redFigure, boardToMousePosition(new Position(i, j));
+	}
+	});
+	shell.open();
+	while (!shell.isDisposed()) {
+	if (!display.readAndDispatch())
+	display.sleep();
+	}
 	}
 
 	/**
-	 * Create contents of the window.
-	 */
+	* Open the window.
+	*/
+	public void open() {
+	Display display = Display.getDefault();
+	createContents();
+	Image image = new Image(display,
+	View.class.getResourceAsStream("board.jpg"));
+
+	Image redFigure = new Image(display,
+	View.class.getResourceAsStream("red_normal.jpg"));
+	GC gc = new GC(image);
+	gc.drawImage(redFigure, 22, 20);
+	gc.drawImage(redFigure, 138, 20);
+	gc.drawImage(redFigure, 254, 20);
+	gc.drawImage(redFigure, 370, 20);
+
+	gc.drawImage(redFigure, 80, 80);
+	gc.drawImage(redFigure, 196, 80);
+	gc.drawImage(redFigure, 310, 80);
+	gc.drawImage(redFigure, 427, 80);
+
+	gc.drawImage(redFigure, 22, 140);
+	gc.drawImage(redFigure, 138, 140);
+	gc.drawImage(redFigure, 254, 140);
+	gc.drawImage(redFigure, 370, 140);
+
+	Button button = new Button(shell, SWT.PUSH);
+	button.setImage(redFigure);
+
+	ImageData img = image.getImageData();
+
+	int x = img.height;
+	int y = img.width;
+
+	System.out.println(x + "," + y);
+
+	Image yellowFigure = new Image(display,
+	View.class.getResourceAsStream("yellow_normal.jpg"));
+	gc.drawImage(yellowFigure, 80, 430);
+	gc.drawImage(yellowFigure, 196, 430);
+	gc.drawImage(yellowFigure, 312, 430);
+	gc.drawImage(yellowFigure, 428, 430);
+
+	gc.drawImage(yellowFigure, 22, 370);
+	gc.drawImage(yellowFigure, 138, 370);
+	gc.drawImage(yellowFigure, 254, 370);
+	gc.drawImage(yellowFigure, 370, 370);
+
+	gc.drawImage(yellowFigure, 80, 310);
+	gc.drawImage(yellowFigure, 196, 310);
+	gc.drawImage(yellowFigure, 312, 310);
+	gc.drawImage(yellowFigure, 428, 310);
+
+	public void drawImages()
+	{
+	  for (int i = 0; i < 8; i ++)
+	     {
+	      for (int j = 0; j < 8; j++)
+	       {
+	      if (m_board[i][j] == 1)
+	       gc.drawImage(blackImage,
+	       BOARD_OFFSET_X + FIGURE_HEIGHT * i,
+	       BOARD_OFFSET_Y + FIGURE_WIDTH * j);
+	      
+	       if (m_board[i][j] == 2)
+	       gc.drawImage(whiteImage,
+	       BOARD_OFFSET_X + FIGURE_HEIGHT * i,
+	       BOARD_OFFSET_Y + FIGURE_WIDTH * j);
+	       }
+	      }
+	}
+	for (int i = 0; i < 8; i++)
+	   for (int j = 0; j < 8; j++) 
+	   {
+	    Rectangle r = new Rectangle(BOARD_OFFSET_X + FIGURE_HEIGHT * i,
+	    BOARD_OFFSET_Y + FIGURE_WIDTH * j, FIGURE_HEIGHT,
+	    FIGURE_WIDTH);
+
+	    if (r.contains(new Point(i, j))) 
+	    {
+	    // we have clicked on figure (i, j)
+	    }
+	   }
+
+	shell.addMouseMoveListener(new MouseMoveListener()
+	{
+	public void mouseMove(MouseEvent arg0) 
+	{
+	System.out.println(arg0.x + " , " + arg0.y);
+	}
+	});
+
+	gc.dispose();
+	
+
+	shell.setBackgroundImage(image);
+	shell.open();
+	shell.layout();
+	while (!shell.isDisposed()) {
+	if (!display.readAndDispatch()) {
+	display.sleep();
+	}
+	}
+	}
+
+	Position boardToMousePosition(Position boardPosition) {
+	Position p = new Position();
+	p.x = BOARD_OFFSET_X + FIGURE_WIDTH * boardPosition.x;
+	p.y = BOARD_OFFSET_Y + FIGURE_HEIGHT * boardPosition.y;
+	return p;
+	}
+
+	Position mouseToBoardPosition(Position mousePosition) {
+	Position p = new Position();
+	p.x = (mousePosition.x - BOARD_OFFSET_X) / FIGURE_WIDTH;
+	p.y = (mousePosition.y - BOARD_OFFSET_Y) / FIGURE_HEIGHT;
+	return p;
+	}
+
+	/**
+	* Create contents of the window.
+	*/
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(498, 525);
-		shell.setText("Checkers Game");
+	shell = new Shell();
+	shell.setSize(495, 525);
+	shell.setText("Checkers Game");
 
 	}
 
-}
+	}
