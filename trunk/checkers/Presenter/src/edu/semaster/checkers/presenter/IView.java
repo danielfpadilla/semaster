fe4cf7a51346fa9cdf331545895bff7e15cec442
abdihusseinow@gmail.com <abdihusseinow@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-11-24 16:54:20 +0000

    added several unimplemented methods to the IView

--- trunk/checkers/Presenter/src/edu/semaster/checkers/presenter/IView.java
+++ trunk/checkers/Presenter/src/edu/semaster/checkers/presenter/IView.java
@@ -1,6 +1,12 @@
 package edu.semaster.checkers.presenter;
 
+
+
 public interface IView {
 	public void setFigurePosition(Point p);
-	public Point getFigurePosition();
+	public void setFigureType(FigureType type);
+	public void processInputActionHandler(IFigureMoveHandler handler);
+	public void refreshUserInterface();
+	public void highlightClickedSquarePosition();
+	public void setStatusMessage(String message);
 }
