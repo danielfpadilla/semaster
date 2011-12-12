7a11d73cf5ec7a390d504d3984ecd3dfa59cc38f
shabee.bakari@gmail.com <shabee.bakari@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-12-12 20:42:31 +0000

    After cleanup and made figures to show initially

--- checkers/View/src/edu/semaster/checkers/view/View.java
+++ checkers/View/src/edu/semaster/checkers/view/View.java
@@ -1,24 +1,21 @@
 package edu.semaster.checkers.view;
-
+import edu.semaster.checkers.baseProject.Piece;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.events.MouseListener;
-import org.eclipse.swt.events.MouseMoveListener;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 
-import edu.semaster.checkers.presenter.FigureType;
-import edu.semaster.checkers.presenter.IView;
-import edu.semaster.checkers.presenter.Presentation;
+
+import edu.semaster.checkers.presentatation.IView;
+import edu.semaster.checkers.presentatation.Point;
+import edu.semaster.checkers.presentatation.Presenter;
+
 
 public class View implements IView {
 	static int BOARD_OFFSET_X = 15;
@@ -27,13 +24,13 @@ public class View implements IView {
 	static int FIGURE_WIDTH = 60;
 
 	private Shell shell;
-	private FigureType[][] m_board;
+	private Piece[][] m_board = new Piece[8][8];;
 	private Image m_boardImage;
 	private Image m_redFigure;
 	private Image m_yellowFigure;
-	private Presentation m_presenter;
+	private Presenter m_presenter;
 	
-	private Position m_highlightedPosition = new Position(-1, -1);
+	private Point m_highlightedPosition = new Point(-1, -1);
 
 	public static void main(String[] args) {
 		try {
@@ -49,14 +46,13 @@ public class View implements IView {
 		Display display = Display.getDefault();
 		createContents();
 		loadFigures(display);
-		initializeBoard();
-		
-		m_presenter = new Presentation(this);
+				
+		m_presenter = new Presenter(this);
 
 		shell.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
-				onClick(new Position(e.x, e.y));
+				onClick(new Point(e.x, e.y));
 			}
 		});
 
@@ -80,26 +76,26 @@ public class View implements IView {
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 
-				if (m_board[i][j].getFigureType() == FigureType.Type.BLACK)
+				if (m_board[i][j].getColor() == Piece.Color.BLACK)
 					gc.drawImage(m_redFigure, BOARD_OFFSET_X
 							+ FIGURE_HEIGHT * j, BOARD_OFFSET_Y
 							+ FIGURE_WIDTH * i);
 
-				if (m_board[i][j].getFigureType() == FigureType.Type.WHITE)
+				if (m_board[i][j].getColor() == Piece.Color.WHITE)
 					gc.drawImage(m_yellowFigure, BOARD_OFFSET_X
 							+ FIGURE_HEIGHT * j, BOARD_OFFSET_Y
 							+ FIGURE_WIDTH * i);
 			}
 		}
 		
-		if (m_highlightedPosition.getx() != -1)
+		if (m_highlightedPosition.getX() != -1)
 		{
 			gc.setForeground(Display.getDefault().getSystemColor(SWT.COLOR_YELLOW));
 			gc.setLineWidth(5);
 			gc.drawRectangle(
 					BOARD_OFFSET_X
-					+ FIGURE_HEIGHT * m_highlightedPosition.getx(), BOARD_OFFSET_Y
-					+ FIGURE_WIDTH * m_highlightedPosition.gety(),
+					+ FIGURE_HEIGHT * m_highlightedPosition.getX(), BOARD_OFFSET_Y
+					+ FIGURE_WIDTH * m_highlightedPosition.getY(),
 					
 					
 					FIGURE_WIDTH, FIGURE_HEIGHT);
@@ -109,25 +105,25 @@ public class View implements IView {
 		}
 	}
 	
-	private void onClick(Position position)
+	private void onClick(Point position)
 	{
-		Position pos = mouseToBoardPosition(position);
+		Point pos = mouseToBoardPosition(position);
 		if (pos.x >= 0 && pos.x < 8 &&
 				pos.y >= 0 && pos.y < 8)
 		{
-			m_presenter.onBoardPositionClicked(new edu.semaster.checkers.presenter.Point(pos.x, pos.y));
+			m_presenter.onBoardPositionClicked(new Point(pos.x, pos.y));
 		}
 	}
 
-	private Position boardToMousePosition(Position boardPosition) {
-		Position p = new Position();
-		p.x = BOARD_OFFSET_X + FIGURE_WIDTH * boardPosition.x;
-		p.y = BOARD_OFFSET_Y + FIGURE_HEIGHT * boardPosition.y;
+	private Point boardToMousePosition(Point boardPosition) {
+		Point p = new Point();
+		p.setPoint(BOARD_OFFSET_X + FIGURE_WIDTH * boardPosition.x,
+				BOARD_OFFSET_Y + FIGURE_HEIGHT * boardPosition.y);
 		return p;
 	}
 
-	private Position mouseToBoardPosition(Position mousePosition) {
-		Position p = new Position();
+	private Point mouseToBoardPosition(Point mousePosition) {
+		Point p = new Point();
 		p.x = (mousePosition.x - BOARD_OFFSET_X) / FIGURE_WIDTH;
 		p.y = (mousePosition.y - BOARD_OFFSET_Y) / FIGURE_HEIGHT;
 		return p;
@@ -156,39 +152,11 @@ public class View implements IView {
 		ImageData data = m_redFigure.getImageData();
 	}
 
-	private void initializeBoard() {
-		m_board = new FigureType[8][8];
-		
-		for (int i = 0; i < 8; i++)
-			for (int j = 0; j < 8; j++)
-				m_board[i][j] = getDefaultFigure(i, j);//new FigureType(FigureType.Type.NONE);
-			
-	}
-
-	private FigureType getDefaultFigure(int x, int y) {
-
-		if (x < 3) {
-			if (!((x % 2 == 0 && y % 2 != 0)
-					|| (x % 2 != 0 && y % 2 == 0))) {
-				return new FigureType(FigureType.Type.BLACK);
-			}
-		}
-
-		if (x > 4) {
-
-			if (!((x % 2 == 0 && y % 2 != 0)
-					|| (x % 2 != 0 && y % 2 == 0))) {
-				return new FigureType(FigureType.Type.WHITE);
-			}
-		}
-
-		return new FigureType(FigureType.Type.NONE);
-	}
-
+	
 	@Override
-	public void setFigurePosition(edu.semaster.checkers.presenter.Point p,
-			FigureType type) {
-		m_board[p.getX()][p.getY()] = type;
+	public void setFigurePosition(Point p,
+			Piece color) {
+		m_board[p.getX()][p.getY()] = color;
 		
 	}
 
@@ -206,13 +174,12 @@ public class View implements IView {
 	}
 
 	@Override
-	public void highlightClickedSquarePosition(
-			edu.semaster.checkers.presenter.Point p, boolean highlighted) {
+	public void highlightClickedSquarePosition(Point p, boolean highlighted) {
 		if (p == null)
 		{
 			m_highlightedPosition = null;
 		}
-		m_highlightedPosition = new Position(p.getX(), p.getY());
+		m_highlightedPosition = new Point(p.getX(), p.getY());
 		// TODO Auto-generated method stub
 		
 	}
