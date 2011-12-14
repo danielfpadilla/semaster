70b8d5356e72d65d523f2379a1d9bda544bcaf60
abdihusseinow@gmail.com <abdihusseinow@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2011-12-14 09:31:17 +0000

    Latest ModelTests after project reconstruction

---
+++ trunk/CheckersProject/ModelTests/src/edu/semaster/checkers/modelTests/BoardTests.java
@@ -0,0 +1,56 @@
+package edu.semaster.checkers.modelTests;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import edu.semaster.checkers.baseProject.Piece;
+import edu.semaster.checkers.model.Board;
+
+
+
+
+
+public class BoardTests {
+
+	@Test
+	public void boardIsNotNull() {
+		Board board = new Board(8, 8);
+		assertNotNull(board);
+	}
+
+	@Test
+	public void checkForPositionsOfWhitePawns() {
+		Board board = new Board(8, 8);
+		board.InitializeBoard(); // assertTrue(Piece.Color.BLACK);
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void checkWhetherAnExceptionIsThrownWhenRowcountAndColumnCount()
+			throws Exception {
+		new Board(0, 0);
+	}
+
+	@Test
+	public void PieceIsNotNull()
+	{
+		Piece mPiece = new Piece(edu.semaster.checkers.baseProject.Piece.Color.BLACK);
+		assertNotNull(mPiece);
+	}
+
+	@Test
+	public void boardPaintedBlackAtPos01() {
+		Board b = new Board(8, 8);
+		assertTrue(b.isPositionPainted(2, 1));
+	}
+
+	@Test
+	public void boardPaintedBlackAtPos00() {
+		Board b = new Board(8, 8);
+		assertFalse(b.isPositionPainted(0, 0));
+	}
+
+}
