5d1aee9e2e07c972a0562afd66c610ded6d7d66c
shabee.bakari@gmail.com <shabee.bakari@gmail.com@25fb90ba-7516-cf1e-4480-91ab63ab2082>
2012-04-12 20:44:05 +0000

    fixed the bug

--- branches/Shaban.Bakari/CheckersProject/Model/src/edu/semaster/checkers/model/Board.java
+++ branches/Shaban.Bakari/CheckersProject/Model/src/edu/semaster/checkers/model/Board.java
@@ -123,27 +123,57 @@ public class Board
 			currentPlayer = Player.PLAYER_WHITE;
 			System.out.println("type -->  YELLOW_KING");
 		}
+		
+		
 
 	}
 
 	public boolean isAValidMove(Point source, Point destination)
 	{
+		boolean result = isAValidMoveInternal(source, destination);
+		System.out.println("valid = " + result);
+		return result;
+	}
+	public boolean isAValidMoveInternal(Point source, Point destination)
+	{
+		
+		
 		if (!isPositionPainted(source.getX(), source.getY()))
 			return false;
+		
+		if((getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.WHITE) && source.getY()==0 )
+		{
+			changePawnToKing(source);
+			System.out.println("changePawnToKing method has been called ");
+			return true;
+		}
+		
+		if((getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.BLACK) && source.getY()==7)
+		{
+			changePawnToKing(source);
+			System.out.println("changePawnToKing method has been called ");
+			return true;
+		}
 
 		if (!(getFigureTypeAt(destination.getX(), destination.getY())
 				.getFigureType() == Type.NONE))
 			return false;
+		
 
 		if (getFigureTypeAt(source.getX(), source.getY())
-				.getFigureType() == Type.WHITE)
+				.getFigureType() == Type.WHITE && currentPlayer==Player.PLAYER_WHITE)
 		{
 			if ((source.getX() == destination.getX() + 1 && source
 					.getY() == destination.getY() + 1)
 					|| (source.getX() == destination.getX() - 1 && source
 							.getY() == destination
 							.getY() + 1))
+			{
+				currentPlayer=Player.PLAYER_BLACK;
 				return true;
+			}
 
 			if ((source.getX() == destination.getX() + 2 && source
 					.getY() == destination.getY() + 2)
@@ -151,52 +181,28 @@ public class Board
 							.getY() == destination
 							.getY() + 2))
 			{
-				if (getFigureTypeAt(
-						(source.getX() + destination
-								.getX()) / 2,
-						(source.getY() + destination
-								.getY()) / 2)
-						.getFigureType() == Type.BLACK
-						|| getFigureTypeAt(
-								(source.getX() + destination
-										.getX()) / 2,
-								(source.getY() + destination
-										.getY()) / 2)
-								.getFigureType() == Type.BLACK_KING)
-				{
-					Point p = new Point(
-							(source.getX() + destination
-									.getX()) / 2,
-							(source.getY() + destination
-									.getY()) / 2);
-					setFigurePosition(p, new FigureType(
-							Type.NONE));
-					return true;
-				}
-				return false;
-
-			}
-
-			if (source.getY() == 0)
-			{
-				Point myPoint = new Point(source.getX(),
-						source.getY());
-				setFigurePosition(myPoint, new FigureType(
-						Type.WHITE_KING));
+				
+				pawnJumpMove(source, destination);				
+				currentPlayer=Player.PLAYER_BLACK;
 				return true;
-			}
 
-			return false;
+			}
+			
+			return false;	
 		}
-		if (getFigureTypeAt(source.getX(), source.getY())
-				.getFigureType() == Type.BLACK)
+		
+		else if (getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.BLACK && currentPlayer==Player.PLAYER_BLACK)
 		{
 			if ((source.getX() == destination.getX() + 1 && source
 					.getY() == destination.getY() - 1)
 					|| (source.getX() == destination.getX() - 1 && source
 							.getY() == destination
 							.getY() - 1))
+			{
+				currentPlayer=Player.PLAYER_WHITE;
 				return true;
+			}
 
 			if ((source.getX() == destination.getX() + 2 && source
 					.getY() == destination.getY() - 2)
@@ -204,55 +210,166 @@ public class Board
 							.getY() == destination
 							.getY() - 2))
 			{
-				if (getFigureTypeAt(
-						(source.getX() + destination
-								.getX()) / 2,
-						(source.getY() + destination
-								.getY()) / 2)
-						.getFigureType() == Type.WHITE
-						|| getFigureTypeAt(
-								(source.getX() + destination
-										.getX()) / 2,
-								(source.getY() + destination
-										.getY()) / 2)
-								.getFigureType() == Type.WHITE_KING)
-				{
-					Point p = new Point(
-							(source.getX() + destination
-									.getX()) / 2,
-							(source.getY() + destination
-									.getY()) / 2);
-					setFigurePosition(p, new FigureType(
-							Type.NONE));
-					return true;
-				}
-				return false;
+				
+				pawnJumpMove(source, destination);
+				currentPlayer=Player.PLAYER_WHITE;
+				return true;	
 			}
-
-			if (source.getY() == 7)
-			{
-				Point myPoint = new Point(source.getX(),
-						source.getY());
-				setFigurePosition(myPoint, new FigureType(
-						Type.BLACK_KING));
-				return true;
-			}
-
+			
 			return false;
+
 		}
+		
+		else if (getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.BLACK_KING && currentPlayer==Player.PLAYER_BLACK)
+		{
 
 		if (getFigureTypeAt(source.getX(), source.getY())
 				.getFigureType() == Type.BLACK_KING
 				&& (Math.abs(source.getX() - destination.getX()) == 1 && Math
 						.abs(source.getY()
 								- destination.getY()) == 1))
+		{
+			currentPlayer=Player.PLAYER_WHITE;
 			return true;
+		}
 
 		if (getFigureTypeAt(source.getX(), source.getY())
 				.getFigureType() == Type.BLACK_KING
 				&& (Math.abs(source.getX() - destination.getX()) > 1 && Math
 						.abs(source.getY()
 								- destination.getY()) > 1))
+		{
+			
+			kingJumpMove(source, destination);
+			currentPlayer=Player.PLAYER_WHITE;
+			return true;		
+		}
+		return false;
+		}
+		
+		else if (getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.WHITE_KING && currentPlayer==Player.PLAYER_WHITE)
+		{
+
+		if (getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.WHITE_KING
+				&& (Math.abs(source.getX() - destination.getX()) == 1 && Math
+						.abs(source.getY()
+								- destination.getY()) == 1))
+		{
+			currentPlayer=Player.PLAYER_BLACK;
+			return true;
+		}
+		if (getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.WHITE_KING
+				&& (Math.abs(source.getX() - destination.getX()) > 1 && Math
+						.abs(source.getY()
+								- destination.getY()) > 1))
+		{
+			
+				kingJumpMove(source, destination);
+				currentPlayer=Player.PLAYER_BLACK;
+				return true;
+		}
+
+		return false;
+		}
+		return true;
+	}
+	
+	
+	private void changePawnToKing(Point p)
+	{
+		if(p.getY()==0)
+		{
+			Point myPoint = new Point(p.getX(),
+					p.getY());
+			setFigurePosition(myPoint, new FigureType(
+					Type.WHITE_KING));
+			System.out.println("set position is called");
+			
+		}
+		else if(p.getY()==7)
+		{
+			Point myPoint = new Point(p.getX(),
+					p.getY());
+			setFigurePosition(myPoint, new FigureType(
+					Type.BLACK_KING));
+			System.out.println("set position is called");
+		}
+		
+	}
+	
+	public void pawnJumpMove(Point source, Point destination)
+	{
+		if (getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.WHITE)
+		{
+			
+
+			if (getFigureTypeAt(
+					(source.getX() + destination
+							.getX()) / 2,
+					(source.getY() + destination
+							.getY()) / 2)
+					.getFigureType() == Type.BLACK
+					|| getFigureTypeAt(
+							(source.getX() + destination
+									.getX()) / 2,
+							(source.getY() + destination
+									.getY()) / 2)
+							.getFigureType() == Type.BLACK_KING)
+			{
+				
+				Point p = new Point(
+						(source.getX() + destination
+								.getX()) / 2,
+						(source.getY() + destination
+								.getY()) / 2);
+				setFigurePosition(p, new FigureType(
+						Type.NONE));
+				
+			}
+		
+		}
+		
+		else if (getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.BLACK)
+		{
+			
+			if (getFigureTypeAt(
+					(source.getX() + destination
+							.getX()) / 2,
+					(source.getY() + destination
+							.getY()) / 2)
+					.getFigureType() == Type.WHITE
+					|| getFigureTypeAt(
+							(source.getX() + destination
+									.getX()) / 2,
+							(source.getY() + destination
+									.getY()) / 2)
+							.getFigureType() == Type.WHITE_KING)
+			{
+				Point p = new Point(
+						(source.getX() + destination
+								.getX()) / 2,
+						(source.getY() + destination
+								.getY()) / 2);
+				
+				setFigurePosition(p, new FigureType(
+						Type.NONE));	
+				
+			}
+		
+		}
+		
+	}
+	
+	public void kingJumpMove(Point source, Point destination)
+	{
+		if (getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.BLACK_KING)
 		{
 			if (getFigureTypeAt(
 					(source.getX() + destination.getX()) / 2,
@@ -270,25 +387,14 @@ public class Board
 								.getX()) / 2,
 						(source.getY() + destination
 								.getY()) / 2);
+				
 				setFigurePosition(p, new FigureType(Type.NONE));
-				return true;
+				
+				
 			}
-			return false;
 		}
-
-		if (getFigureTypeAt(source.getX(), source.getY())
-				.getFigureType() == Type.WHITE_KING
-				&& (Math.abs(source.getX() - destination.getX()) == 1 && Math
-						.abs(source.getY()
-								- destination.getY()) == 1))
-		{
-			return true;
-		}
-		if (getFigureTypeAt(source.getX(), source.getY())
-				.getFigureType() == Type.WHITE_KING
-				&& (Math.abs(source.getX() - destination.getX()) > 1 && Math
-						.abs(source.getY()
-								- destination.getY()) > 1))
+		else if (getFigureTypeAt(source.getX(), source.getY())
+				.getFigureType() == Type.WHITE_KING)
 		{
 			if (getFigureTypeAt(
 					(source.getX() + destination.getX()) / 2,
@@ -306,13 +412,46 @@ public class Board
 								.getX()) / 2,
 						(source.getY() + destination
 								.getY()) / 2);
+				
 				setFigurePosition(p, new FigureType(Type.NONE));
-				return true;
+				
+				
 			}
-			return false;
-
 		}
+			
+	}
 
-		return true;
+	public boolean isGameOver()
+	{
+		int redFigures = 0, yellowFigures = 0;
+		for (int i = 0; i < 8; i++)
+		{
+
+			for (int j = 0; j < 8; j++)
+			{
+				if(getFigureTypeAt(i, j).getFigureType()==Type.BLACK)
+					redFigures++;
+				else if(getFigureTypeAt(i, j).getFigureType()==Type.BLACK_KING)
+					redFigures++;
+				else if(getFigureTypeAt(i, j).getFigureType()==Type.WHITE)
+					yellowFigures++;
+				else if(getFigureTypeAt(i, j).getFigureType()==Type.WHITE_KING)
+					yellowFigures++;
+			}
+		}
+		
+		if(redFigures==0)
+		{
+			System.out.println("Yellow won!");
+			return true;
+		}
+		else if(yellowFigures==0)
+		{
+			System.out.println("Red won");
+			return true;
+		}
+		
+		
+		return false;
 	}
 }
